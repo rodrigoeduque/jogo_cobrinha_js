bdc05f29c8a1878b9b5d9aa4640e8f062050bb1e
rodrigoeduque <rodrigoeduque@hotmail.com>
2020-12-04 15:23:42 -0300

    snake criada

--- asserts/javascript/script.js
+++ asserts/javascript/script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box, 
+    y: 8 * box
+}
 
 
 function criarBG() {
@@ -8,4 +13,13 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
+function CriarCobrinha(){
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = "black";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+        
+    }
+}
+
 criarBG();
+CriarCobrinha();
