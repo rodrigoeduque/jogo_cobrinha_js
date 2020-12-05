0f8623ba313b4da4eed6c832364c3316b3dceea3
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2020-12-05 13:20:17 -0300

    criar comida

--- asserts/javascript/script.js
+++ asserts/javascript/script.js
@@ -9,6 +9,11 @@ snake[0] = {
 
 let direction = "right";
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1 ) * box,
+    y: Math.floor(Math.random() * 15 + 1 ) * box
+}
+
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -21,6 +26,11 @@ function CriarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
 
     }
+}
+
+function drawFood (){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 
@@ -73,6 +83,7 @@ function iniciarJogo() {
         x: snakeX,
         y: snakeY
     }
+    drawFood();
 
     snake.unshift(newHead);
 
